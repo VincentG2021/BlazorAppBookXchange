8a612ddf778a6a2962025436e5ac31cadbc53e7d
VincentG <vincentgade@gmail.com>
2022-07-01 15:28:23 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorAppBookXchange/Models/LivreModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppBookXchange.Models
+{
+    public class LivreModel
+    {
+        public int IdLivre { get; set; }
+        public string Titre { get; set; }
+
+        public string Auteur { get; set; }
+
+        public string Synopsis { get; set; }
+
+    }
+}
---
+++ BlazorAppBookXchange/Pages/BookList/BookList.cs
@@ -0,0 +1,18 @@
+﻿using BlazorAppBookXchange.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorAppBookXchange.Pages.BookList
+{
+    public partial class BookList
+    {
+        [Inject]
+        HttpClient Http { get; set; }
+
+        public List<LivreModel> bookList = new List<LivreModel>();
+        protected override async Task OnInitializedAsync()
+        {
+            bookList = await Http.GetFromJsonAsync<List<LivreModel>>("https://localhost:7003/api/BookXchangeAPI/GetBookList");
+        }
+    }
+}
