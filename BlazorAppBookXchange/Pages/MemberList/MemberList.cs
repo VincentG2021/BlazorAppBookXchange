1785ed816ccebb7804ac247992c95a46baca114d
VincentG <vincentgade@gmail.com>
2022-07-07 14:00:43 +0200

    Ajout lien memberlist dans NavMenu, MembreModel, MemberList dans Pages

---
+++ BlazorAppBookXchange/Pages/MemberList/MemberList.cs
@@ -0,0 +1,19 @@
+﻿using BlazorAppBookXchange.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorAppBookXchange.Pages.MemberList
+{
+    public partial class MemberList
+    {
+        [Inject]
+        HttpClient Http { get; set; }
+
+        public List<MembreModel> memberList = new List<MembreModel>();
+        protected override async Task OnInitializedAsync()
+        {
+            memberList = await Http.GetFromJsonAsync<List<MembreModel>>("https://localhost:7144/api/BookXchangeAPI/GetMemberList");
+        }
+
+    }
+}
