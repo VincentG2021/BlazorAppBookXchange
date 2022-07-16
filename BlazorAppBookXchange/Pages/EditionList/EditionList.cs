f8686fbee31d61f837e7fd680d7f160cb4e8b02d
VincentG <vincentgade@gmail.com>
2022-07-16 15:51:39 +0200

    Essai affichage Selected book depuis BookList dans EditionList

---
+++ BlazorAppBookXchange/Pages/EditionList/EditionList.cs
@@ -0,0 +1,12 @@
+﻿using BlazorAppBookXchange.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppBookXchange.Pages.EditionList
+{
+    public partial class EditionList
+    {
+        [Parameter]
+        public LivreModel Selected { get; set; }
+
+    }
+}
