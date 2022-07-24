44c0031c5763f982536b5c01f7f1d886fb8bcf49
VincentG <vincentgade@gmail.com>
2022-07-24 16:19:37 +0200

    Ajout ModalDialog, CreateExemplaire et DeleteExemplaire

---
+++ BlazorAppBookXchange/Components/ExemplaireComponents/ExemplaireLists/ExemplaireListByLivre.cs
@@ -0,0 +1,75 @@
+﻿using BlazorAppBookXchange.Models;
+using BlazorAppBookXchange.Tools;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppBookXchange.Components.ExemplaireComponents.ExemplaireLists
+{
+    public partial class ExemplaireListByLivre
+    {
+        [Inject] private ApiRequester _requester { get; set; }
+        [Inject] private NavigationManager _navigationManager { get; set; }
+        [Inject] private AccountManager _accountManager { get; set; }
+
+        //public List<ExemplaireModel> exemplairesList = new List<ExemplaireModel>();
+
+        public List<ExemplaireModel> exemplairesByLivre = new List<ExemplaireModel>();
+        [Parameter] public LivreModel SelectedBook { get; set; }
+        [Parameter] public int IdSelectedBook { get; set; }
+        [Parameter] public string? TitreSelectedBook { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            bool isTokenPresent = await _accountManager.checkIfTokenStored();
+            if (!_accountManager.IsConnected && !isTokenPresent)
+            {
+                _navigationManager.NavigateTo("/login");
+                return;
+            }
+
+            await LoadData();
+
+            await _accountManager.checkIfTokenStored();
+            _accountManager.OnChange += StateHasChanged;
+
+            //AVEC [Autorize("isConnected")] dans ExemplaireApi:
+
+            //bool isTokenPresent = await accountManager.checkIfTokenStored();
+            //if (!accountManager.IsConnected && !isTokenPresent)
+            //{
+            //    navigationManager.NavigateTo("/login");
+            //    return;
+            //}
+            //string Token = await accountManager.GetToken("token");
+            ////exemplairesList = await Http.GetFromJsonAsync<List<ExemplaireModel>>("Exemplaire/GetAllExemplairesList");
+            //exemplairesList = await _requester.Get<List<ExemplaireModel>>("Exemplaire/GetAllExemplairesList", Token);
+
+            //SANS[Autorize("isConnected")], AVEC[AllowAnonymous] dans BookXchangeBE.API:
+            //exemplairesList = await _requester.Get<List<ExemplaireModel>>("Exemplaire/GetAllExemplairesList");
+            //pidSelectedEdition = IdSelectedEdition;
+        }
+
+        private async Task LoadData()
+        {
+            await GetExemplairesByLivre();
+            StateHasChanged();
+        }
+
+        public async Task<List<ExemplaireModel>> GetExemplairesByLivre()
+        {
+            await _accountManager.checkIfTokenStored();
+            string Token = await _accountManager.GetToken("token");
+            exemplairesByLivre = await _requester.Get<List<ExemplaireModel>>($"Exemplaire/GetExemplaireByLivre/{IdSelectedBook}", Token);
+            return exemplairesByLivre;
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            IdSelectedBook = SelectedBook.IdLivre;
+            await GetExemplairesByLivre();
+        }
+
+        public void Dispose()
+        {
+            _accountManager.OnChange -= StateHasChanged;
+        }
+    }
+}
--- BlazorAppBookXchange/Models/ExemplaireModel.cs
+++ BlazorAppBookXchange/Models/ExemplaireModel.cs
@@ -1,9 +1,84 @@
-﻿namespace BlazorAppBookXchange.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppBookXchange.Models
 {
     public class ExemplaireModel : EditionModel
     {
         public int IdExemplaire { get; set; }
         public int IdMembre { get; set; }
         public int IdEdition { get; set; }
+        public bool IsRowExpanded { get; set; } = false;
+
     }
+
+    public class CreateExemplaireModel : EditionModel
+    {
+        public int IdMembre { get; set; }
+
+        [DisplayName("Titre de l'ouvrage")]
+        [Required]
+        public string Titre { get; set; }
+
+        [DisplayName("Auteur l'ouvrage")]
+        [Required]
+        public string Auteur { get; set; }
+
+        [DisplayName("Résumé, synopsis")]
+        [Required]
+        public string Synopsis { get; set; }
+
+        [DisplayName("Isbn")]
+        [Required]
+        public string Isbn { get; set; }
+
+        [DisplayName("Date de parution")]
+        [Required]
+        public DateTime Parution { get; set; } = DateTime.UtcNow;
+
+        [DisplayName("Format")]
+        [Required]
+        public string Format { get; set; }
+
+        //[DisplayName("Disponibilité")]
+        //[Required]
+        //public bool? Disponible { get; set; }
+
+    }
+
+
+    public class EditExemplaireLivreModel : EditionModel
+    {
+        public int IdExemplaire { get; set; }
+
+        [DisplayName("Titre de l'ouvrage")]
+        [Required]
+        public string Titre { get; set; }
+
+        [DisplayName("Auteur l'ouvrage")]
+        [Required]
+        public string Auteur { get; set; }
+
+        [DisplayName("Résumé, synopsis")]
+        [Required]
+        public string Synopsis { get; set; }
+
+        [DisplayName("Isbn")]
+        [Required]
+        public string Isbn { get; set; }
+
+        [DisplayName("Date de parution")]
+        [Required]
+        public DateTime Parution { get; set; }
+
+        [DisplayName("Format")]
+        [Required]
+        public string Format { get; set; }
+
+        //[DisplayName("Disponibilité")]
+        //[Required]
+        //public bool? Disponible { get; set; }
+
+    }
+
 }
